fc67507533968f0a15529aa7759844e8036b9cbe
Andres Nedilskyj <an3developer@gmail.com>
2021-02-25 21:57:34 -0800

    Added champion selection UI

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -9,17 +9,37 @@ public class Card : MonoBehaviour
     public Image championImage, cardFrame;
     public TextMeshProUGUI[] traitsText;
     public Image[] traitImages;
+    public GameObject clickBlocker;
 
+    bool isSelected = false;
+    string name;
     Trait[] traits;
     int cost;
+    int indexInSelectionList = 0;
+    int indexInDeck = 0;
     public void OnClickCard()
     {
-        print("Clicked card");
-        // trigger method in game manager to pick card
+        GameManager.Instance.OnCardClicked(indexInDeck);
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void SetIndexInSelectionList(int index)
+    {
+        indexInSelectionList = index;
+    }
+
+    public void SetIndexInDeck(int index)
+    {
+        indexInDeck = index;
     }
 
     public void SetName(string name)
     {
+        this.name = name;
         cardNameText.text = name;
     }
 
@@ -27,15 +47,49 @@ public class Card : MonoBehaviour
     {
         championImage.sprite = image;
     }
+
     public void SetCost(int cost)
     {
         this.cost = cost;
+        if (cardFrame == null)
+            return;
         cardFrame.sprite = UIStorage.GetCardFrames()[cost - 1];
+    }
+
+    public void OnSelect()
+    {
+        isSelected = true;
+        GameManager.Instance.OnSelectedChampion(indexInSelectionList);
+    }
+
+    public void OnDeselect()
+    {
+        isSelected = false;
+        GameManager.Instance.OnDeselectedChampion(indexInSelectionList);
+    }
+
+    public void DisableSelection()
+    {
+        if (!isSelected)
+        {
+            clickBlocker.gameObject.SetActive(true);
+        }
+    }
+
+    public void EnableSelection()
+    {
+        if (!isSelected)
+        {
+            clickBlocker.gameObject.SetActive(false);
+        }
     }
 
     public void SetTraits(Trait[] traits)
     {
         this.traits = traits;
+        if (traitsText.Length == 0 || traitsText == null || traitImages.Length == 0 || traitImages == null)
+            return;
+
         for(int i = 0; i < traits.Length; i++)
         {
             // set trait text
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
-
+    public static GameManager Instance;
     public TextMeshProUGUI levelText;
-    public Card[] cards;
+    public Card[] cardSlotArray; // assign these in the inspector. These are the cards that are in the slots
+    public List<Card> championSelectionCardsList = new List<Card>();
+    public GameObject championSelectionButtonPrefab;
+    public Transform championSelectionButtonParent;
 
     public ChampionCard[] allChampions;
     List<ChampionCard> oneCostChamps, twoCostChamps, threeCostChamps, fourCostChamps, fiveCostChamps;
 
     public int level = 1;
+
+    int maxSelectedCards = 10;
+
+    List<Card> selectedCards = new List<Card>();
 
     int[,] probabilities = new int[,] 
         { 
@@ -24,11 +32,19 @@ public class GameManager : MonoBehaviour
             { 35, 35, 25, 5, 0 }, 
             { 22, 35, 30, 12, 1 },
             { 15, 25, 35, 20, 5 }, 
-            { 10, 15, 30, 30, 15 } // level 9
+            { 10, 15, 30, 30, 15 } // level 9   
         };
 
     private void Awake()
     {
+        if( Instance != null)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            Instance = this;
+        }
+
         oneCostChamps = new List<ChampionCard>();
         twoCostChamps = new List<ChampionCard>();
         threeCostChamps = new List<ChampionCard>();
@@ -62,6 +78,72 @@ public class GameManager : MonoBehaviour
                 Debug.Log("Missing Image: " + allChampions[i].championName);
             }
         }
+
+        // create champion selection buttons and populate them
+        for(int i = 0; i < allChampions.Length; i++)
+        {
+            Card card = Instantiate(championSelectionButtonPrefab, championSelectionButtonParent).GetComponent<Card>();
+            card.SetCost(allChampions[i].cost);
+            card.SetName(allChampions[i].name);
+            card.SetTraits(allChampions[i].traits);
+            card.SetImage(allChampions[i].image);
+            card.SetIndexInSelectionList(i);
+            championSelectionCardsList.Add(card);
+        }
+    }
+
+    public void OnSelectedChampion(int index)
+    {
+
+        Card card = championSelectionCardsList[index];
+        print("Selected " + card.GetName());
+
+        // add this card to selected cards
+        selectedCards.Add(card);
+
+        if (selectedCards.Count >= maxSelectedCards)
+        {
+            // disable selection
+            DisableCardSelection(true);
+        }
+        // populate and enable card
+
+    }
+
+    void DisableCardSelection(bool disable)
+    {
+        if (disable)
+        {
+            for (int i = 0; i < championSelectionCardsList.Count; i++)
+            {
+                championSelectionCardsList[i].DisableSelection();
+            }
+        } else
+        {
+            for (int i = 0; i < championSelectionCardsList.Count; i++)
+            {
+                championSelectionCardsList[i].EnableSelection();
+            }
+        }
+
+    }
+
+
+    public void OnDeselectedChampion(int index)
+    {
+        Card card = championSelectionCardsList[index];
+        // add this card to selected cards
+        selectedCards.Remove(card);
+
+        // if cards list was previously at max capacity
+        if (selectedCards.Count + 1 >= maxSelectedCards)
+        {
+            // enable selection
+            DisableCardSelection(false);
+        }
+
+        // disable the UI card
+
     }
 
     private void Update()
@@ -71,6 +153,16 @@ public class GameManager : MonoBehaviour
             RefreshCardSlots();
         }
     }
+
+    public void OnCardClicked(int indexInDeck)
+    {
+        print(cardSlotArray[indexInDeck].GetName());
+
+        // do some logic to calculate whether this was a correct click, incorrect click, or missed click
+
+        cardSlotArray[indexInDeck].gameObject.SetActive(false);
+    }
+
     public void OnClickStart() {
         print("Start button clicked");
 
@@ -86,7 +178,7 @@ public class GameManager : MonoBehaviour
     public void RefreshCardSlots()
     {
         // loops through each card slot
-        for (int i = 0; i < cards.Length; i++)
+        for (int i = 0; i < cardSlotArray.Length; i++)
         {
             int cost = 0;
 
@@ -133,12 +225,12 @@ public class GameManager : MonoBehaviour
             // selects random champion from the specific "deck" in which all cards have the same cost
             int randomChamp = Random.Range(0, champions.Length);
 
-            SetCardUI(cards[i], champions[randomChamp]);         
+            SetCardUI(cardSlotArray[i], champions[randomChamp], i);         
         }
     }
 
 
-    void SetCardUI(Card currCard, ChampionCard champ)
+    void SetCardUI(Card currCard, ChampionCard champ, int index)
     {
         currCard.gameObject.SetActive(true);
 
@@ -146,5 +238,6 @@ public class GameManager : MonoBehaviour
         currCard.SetName(champ.championName);
         currCard.SetTraits(champ.traits);
         currCard.SetCost(champ.cost);
+        currCard.SetIndexInDeck(index);
     }
 }
